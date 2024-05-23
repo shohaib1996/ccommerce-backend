67b20e65d5fa337bfe6221d45ac15c970bc86b4e
shohaib1996 <khanshohaibhossain@gmail.com>
2024-05-23 16:36:25 +0600

    some changes in order error messages

--- src/app/modules/product/order.controller.ts
+++ src/app/modules/product/order.controller.ts
@@ -17,7 +17,7 @@ const createOrder = async (req: Request, res: Response) => {
   } catch (err: any) {
     res.status(500).json({
       success: false,
-      message: "Insufficient quantity available in inventory",
+      message: err.message,
     });
   }
 };
