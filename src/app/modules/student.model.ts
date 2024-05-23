9adfb4a5c90b95a29078cf1a8eac25d5ea661dec
shohaib1996 <khanshohaibhossain@gmail.com>
2024-05-23 08:42:39 +0600

    mongose schema is done

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -1,231 +1,78 @@
 import { Schema, model } from "mongoose";
-import validator from "validator";
-const bcrypt = require("bcrypt");
 
-import {
-  TGuardian,
-  TLocalGuardian,
-  TStudent,
-  // StudentMethods,
-  StudentModel,
-  TUserName,
-} from "./student/product.interface";
-import config from "../config";
-
-const userNameSchema = new Schema<TUserName>({
-  firstName: {
+const variantSchema = new Schema({
+  type: {
     type: String,
-    required: [true, "First Name is required"],
+    required: [true, "Variant type is required"],
     trim: true,
-    maxlength: [10, "First name can not be more than 10"],
-    // validate: {
-    //   validator: function (value: string) {
-    //     const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
-    //     return firstNameStr === value;
-    //   },
-    //   message: "{VALUE} is not capitalize format",
-    // },
+    enum: {
+      values: ["color", "size", "style"],
+      message: "Variant type `{VALUE}` is not supported",
+    },
   },
-  middleName: {
+  value: {
     type: String,
+    required: [true, "Variant value is required"],
     trim: true,
-  },
-  lastName: {
-    type: String,
-    trim: true,
-    required: [true, "Last Name is required"],
-    // validate: {
-    //   validator: (value: string) => validator.isAlpha(value),
-    //   message: "{VALUE} is not valid",
-    // },
   },
 });
 
-const guardianSchema = new Schema<TGuardian>({
-  fatherName: {
-    type: String,
-    required: [true, "Father's Name is required"],
+const inventorySchema = new Schema({
+  quantity: {
+    type: Number,
+    required: [true, "Inventory quantity is required"],
+    min: [0, "Inventory quantity cannot be negative"],
+    validate: {
+      validator: Number.isInteger,
+      message: "Inventory quantity must be an integer",
+    },
   },
-  fatherOccupation: {
-    type: String,
-    required: [true, "Father's Occupation is required"],
-  },
-  fatherContactNo: {
-    type: String,
-    required: [true, "Father's Contact Number is required"],
-  },
-  motherName: {
-    type: String,
-    required: [true, "Mother's Name is required"],
-  },
-  motherOccupation: {
-    type: String,
-    required: [true, "Mother's Occupation is required"],
-  },
-  motherContactNo: {
-    type: String,
-    required: [true, "Mother's Contact Number is required"],
+  inStock: {
+    type: Boolean,
+    required: [true, "Stock status is required"],
   },
 });
 
-const localGuardianSchema = new Schema<TLocalGuardian>({
+const productSchema = new Schema({
   name: {
     type: String,
-    required: [true, "Local Guardian's Name is required"],
+    required: [true, "Product name is required"],
+    trim: true,
+    maxlength: [100, "Product name cannot exceed 100 characters"],
   },
-  occupation: {
+  description: {
     type: String,
-    required: [true, "Local Guardian's Occupation is required"],
+    required: [true, "Product description is required"],
+    trim: true,
+    minlength: [10, "Product description must be at least 10 characters long"],
+    maxlength: [1000, "Product description cannot exceed 1000 characters"],
   },
-  contactNo: {
+  price: {
+    type: Number,
+    required: [true, "Product price is required"],
+    min: [0, "Product price cannot be negative"],
+    max: [100000, "Product price cannot exceed 100,000"],
+  },
+  category: {
     type: String,
-    required: [true, "Local Guardian's Contact Number is required"],
-  },
-  address: {
-    type: String,
-    required: [true, "Local Guardian's Address is required"],
-  },
-});
-
-const studentSchema = new Schema<TStudent, StudentModel>(
-  {
-    id: {
-      type: String,
-      required: [true, "ID is required"],
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: [true, "password is required"],
-      maxlength: [20, "Password not more than 20 character"],
-    },
-    name: {
-      type: userNameSchema,
-      required: [true, "Name is required"],
-    },
-    gender: {
-      type: String,
-      enum: {
-        values: ["male", "female", "other"],
-        message: "{VALUE} is not supported as a valid gender",
-      },
-      required: [true, "Gender is required"],
-    },
-    dateOfBirth: {
-      type: Date,
-      required: [true, "Date of Birth is required"],
-    },
-    email: {
-      type: String,
-      required: [true, "Email is required"],
-      unique: true,
-      // validate: {
-      //   validator: (value: string) => validator.isEmail(value),
-      //   message: "{VALUE} is not a valid email address",
-      // },
-    },
-    contactNo: {
-      type: String,
-      required: [true, "Contact Number is required"],
-    },
-    emergencyContactNo: {
-      type: String,
-      required: [true, "Emergency Contact Number is required"],
-    },
-    bloogGroup: {
-      type: String,
-      enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
-      required: [true, "Blood Group is required"],
-    },
-    presentAddress: {
-      type: String,
-      required: [true, "Present Address is required"],
-    },
-    permanentAddres: {
-      type: String,
-      required: [true, "Permanent Address is required"],
-    },
-    guardian: {
-      type: guardianSchema,
-      required: [true, "Guardian information is required"],
-    },
-    localGuardian: {
-      type: localGuardianSchema,
-      required: [true, "Local Guardian information is required"],
-    },
-    profileImg: {
-      type: String,
-    },
-    isActive: {
-      type: String,
-      enum: ["active", "blocked"],
-      default: "active",
-    },
-    isDeleted: {
-      type: Boolean,
-      default: false,
+    required: [true, "Product category is required"],
+    trim: true,
+    enum: {
+      values: ["Electronics", "Clothing", "Books", "Home & Garden", "Other"],
+      message: "Category `{VALUE}` is not supported",
     },
   },
-  {
-    toJSON: {
-      virtuals: true,
+  tags: [
+    {
+      type: String,
+      trim: true,
+      lowercase: true,
     },
-  }
-);
-
-// virtual
-studentSchema.virtual("fullName").get(function () {
-  return this.name.firstName + this.name.middleName + this.name.lastName;
-});
-// pre save middleware / hook : will work on create() save()
-
-studentSchema.pre("save", async function (next) {
-  // console.log(this, "pre hook: we will save the data");
-  const user = this;
-  user.password = await bcrypt.hash(
-    user.password,
-    Number(config.bcrypt_salt_rounds)
-  );
-
-  next();
+  ],
+  variants: [variantSchema],
+  inventory: inventorySchema,
 });
 
-// post save middleware / hook
-studentSchema.post("save", function (doc, next) {
-  doc.password = "";
-  // console.log(doc);
+const Product = model("Product", productSchema);
 
-  next();
-});
-// Query Middleware
-studentSchema.pre("find", function (next) {
-  this.find({ isDeleted: { $ne: true } });
-  next();
-});
-
-studentSchema.pre("findOne", function (next) {
-  this.find({ isDeleted: { $ne: true } });
-  next();
-});
-
-// [ {$match: { isDeleted : {  $ne: : true}}}   ,{ '$match': { id: '123456' } } ]
-
-studentSchema.pre("aggregate", function (next) {
-  this.pipeline().unshift({ $match: { isDeleted: { $ne: true } } });
-  next();
-});
-
-// creating a custom static method
-
-studentSchema.statics.isUserExist = async function (id: string) {
-  const existingUser = await Student.findOne({ id });
-  return existingUser;
-};
-
-// Creating a custom instance method
-// studentSchema.methods.isUserExist = async function (id: string) {
-//   const existingUser = await Student.findOne({ id });
-//   return existingUser;
-// };
-
-export const Student = model<TStudent, StudentModel>("Student", studentSchema);
+export default Product;
